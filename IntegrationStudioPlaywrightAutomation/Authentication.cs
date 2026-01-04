32f1704b99de09bfdc74547f0c62ab3dc15a415f
Tuppulu <sk.ahamadi2000@gmail.com>
2026-01-04 16:20:08 +0530

    Initial Playwright automation project

---
+++ IntegrationStudioPlaywrightAutomation/Authentication.cs
@@ -0,0 +1,41 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+
+
+namespace IntegrationStudioPlaywrightAutomation
+{
+    public class Authentication
+    {
+        [Test]
+        public async Task AuthenticationSetUp()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(
+                                new BrowserTypeLaunchOptions
+                                {
+                                    Headless = false
+                                });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("https://internal.integrationstudio.capdev-connect.aveva.com/");
+            var email = Environment.GetEnvironmentVariable("Authentication_Email");
+            await page.FillAsync("#email", email);
+            await page.ClickAsync("#submit");
+            var password = Environment.GetEnvironmentVariable("Authentication_Password");
+            await page.FillAsync("#i0118", password);
+            await page.ClickAsync("#idSIButton9");
+            await page.ClickAsync("#idSIButton9");
+
+            await page.WaitForURLAsync("**/solutions**");
+            await context.StorageStateAsync(new()
+            {
+                Path = "auth.json"
+            });
+
+            
+            await browser.CloseAsync();
+        }
+    }
+
+}
